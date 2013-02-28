c9ede5150ae84ee656442e3a7561a88558093440
Jen Song <jsong@plos.org>
2013-02-28 14:41:28 -0800

    commiting files that was missed in the previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 AlmReport::Application.routes.draw do
+  get "home/index"
+
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
