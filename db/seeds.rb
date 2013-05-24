8056a69c5498939c5aa35edb675bd9238337ae4f
John Callaway <jcallaway@plos.org>
2013-05-24 12:19:16 -0700

    Add a comment specifying how long this takes to run.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Warning: loading this currently takes between 15 and 50 minutes, depending on the
+# environment (15 for a local box, 50 for an ec2 instance).
 
 require "countries"
 require "csv"
