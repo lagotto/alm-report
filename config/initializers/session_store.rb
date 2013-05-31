49c0187562fe41c04d3c0518b82b98f71adccf54
Jen Song <jsong@plos.org>
2013-05-30 17:55:06 -0700

    removed commented out code

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-#AlmReport::Application.config.session_store :cookie_store, key: '_alm-report_session'
-#AlmReport::Application.config.session_store :cache_store
+# use memcached for session store
 Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.hour
 
