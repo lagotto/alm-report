70ac60978d83b65b3ff9fcd4ab66c9feb051059f
John Callaway <jcallaway@plos.org>
2013-06-20 13:38:47 -0700

    ALM-333: remove validation of all DOIs in a report.  Instead, just remove or ignore them when they are needed, depending on the use.

--- app/models/report.rb
+++ app/models/report.rb
@@ -71,19 +71,23 @@ class Report < ActiveRecord::Base
 
           article_alm_data = alm_data[report_doi.doi]
           article_data = solr_data[report_doi.doi]
-
-          authors = article_data["author_display"].nil? ? "" : article_data["author_display"].join(", ")
-          affiliate = article_data["affiliate"].nil? ? "" : article_data["affiliate"].join("; ")
-
-          csv << [
-            report_doi.doi, article_data["pmid"], article_data["title"], authors, affiliate,
-            article_data["cross_published_journal_name"][0], article_data["publication_date"], article_data["article_type"],
-            article_alm_data[:plos_total], article_alm_data[:plos_html], article_alm_data[:plos_pdf], article_alm_data[:plos_xml],
-            article_alm_data[:pmc_total], article_alm_data[:pmc_views], article_alm_data[:pmc_pdf],
-            article_alm_data[:crossref_citations], article_alm_data[:scopus_citations], article_alm_data[:pmc_citations],
-            article_alm_data[:citeulike], article_alm_data[:mendeley], article_alm_data[:twitter], article_alm_data[:facebook], article_alm_data[:wikipedia],
-            article_alm_data[:research_blogging], article_alm_data[:nature], article_alm_data[:scienceseeker]
-          ]
+          
+          # If the article was unpublished (rare), skip it.
+          if !article_alm_data.nil? && !article_data.nil?
+  
+            authors = article_data["author_display"].nil? ? "" : article_data["author_display"].join(", ")
+            affiliate = article_data["affiliate"].nil? ? "" : article_data["affiliate"].join("; ")
+  
+            csv << [
+              report_doi.doi, article_data["pmid"], article_data["title"], authors, affiliate,
+              article_data["cross_published_journal_name"][0], article_data["publication_date"], article_data["article_type"],
+              article_alm_data[:plos_total], article_alm_data[:plos_html], article_alm_data[:plos_pdf], article_alm_data[:plos_xml],
+              article_alm_data[:pmc_total], article_alm_data[:pmc_views], article_alm_data[:pmc_pdf],
+              article_alm_data[:crossref_citations], article_alm_data[:scopus_citations], article_alm_data[:pmc_citations],
+              article_alm_data[:citeulike], article_alm_data[:mendeley], article_alm_data[:twitter], article_alm_data[:facebook], article_alm_data[:wikipedia],
+              article_alm_data[:research_blogging], article_alm_data[:nature], article_alm_data[:scienceseeker]
+            ]
+          end
         end
       end
 
