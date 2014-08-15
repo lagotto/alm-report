53c9c3946e7ff23576469cbe1f62c472b0ddfe8a
Jure Triglav <juretriglav@gmail.com>
2014-08-15 23:14:41 +0200

    Removing PLOS internal IPs

--- test/unit/ip_ranges_test.rb
+++ test/unit/ip_ranges_test.rb
@@ -5,18 +5,11 @@ class IpRangesTest < ActiveSupport::TestCase
   
   test "is_internal_ip_test" do
     assert(IpRanges.is_internal_ip("127.0.0.1"))
-    assert(IpRanges.is_internal_ip("***REMOVED***98"))
-    assert(IpRanges.is_internal_ip("***REMOVED***97"))
-    assert(IpRanges.is_internal_ip("***REMOVED***126"))
-    assert(IpRanges.is_internal_ip("***REMOVED***217"))
-    assert(IpRanges.is_internal_ip("***REMOVED***222"))
     assert(IpRanges.is_internal_ip("10.38.70.212"))
     assert(IpRanges.is_internal_ip("172.20.13.127"))
     assert(IpRanges.is_internal_ip("192.168.14.2"))
     
     assert(!IpRanges.is_internal_ip("37.134.13.56"))
-    assert(!IpRanges.is_internal_ip("***REMOVED***95"))
-    assert(!IpRanges.is_internal_ip("***REMOVED***128"))
     assert(!IpRanges.is_internal_ip("***REMOVED***215"))
     assert(!IpRanges.is_internal_ip("172.32.1.1"))
     assert(!IpRanges.is_internal_ip("192.169.22.178"))
