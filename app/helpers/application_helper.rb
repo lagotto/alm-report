4f036055beed252c5fbb1c0e63bfbc103376e1d1
Jure Triglav <juretriglav@gmail.com>
2014-08-30 18:34:16 +0200

    Cleanup template switching with method_missing and individual slim templates.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,44 +1,19 @@
 module ApplicationHelper
-  def logo
-    src = case APP_CONFIG['mode']
-    when 'plos'
-      'logo.plos-alm.png'
-    when 'default'
-      (image_tag 'logo.alm.svg',
-        alt: 'Public Library of Science', height: '48px') +
-      (content_tag(:strong, 'ALM')+ " Reports")
-    end
 
-  end
-
-  def footer_logo
-    src = case APP_CONFIG['mode']
-    when 'plos'
-      'footer.logo.plos-alm.png'
-    when 'default'
-      'footer.logo.alm.png'
-    end
-    image_tag src, :alt => 'Public Library of Science'
-  end
-
-  def font
+  def switch(template)
     case APP_CONFIG['mode']
     when 'plos'
-      javascript_tag <<-SCRIPT
-        WebFontConfig = { fontdeck: { id: '24557' } };
-
-        (function() {
-          var wf = document.createElement('script');
-          wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-          '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-          wf.type = 'text/javascript';
-          wf.async = 'true';
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(wf, s);
-        })();
-      SCRIPT
+      render "home/plos/#{template}"
     when 'default'
-      stylesheet_link_tag 'http://fonts.googleapis.com/css?family=Fira+Sans:300,500,300italic,500italic'
+      render "home/default/#{template}"
+    end
+  end
+
+  def method_missing(template)
+    if %i[logo footer_logo font].include?(template)
+      switch(template)
+    else
+      super
     end
   end
 end
