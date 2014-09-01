4c7cd9fb44752483b75e7698d6a6654336b963dd
Jure Triglav <juretriglav@gmail.com>
2014-09-01 02:57:17 +0200

    Add new simple spec for HomeController's add-articles action.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -8,4 +8,14 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test "should find articles" do
+    stub_request(:get,
+      "http://api.plos.org/search?author=&author_country=&commit=Search&cross_published_journal_name=All%20Journals&datepicker1=&datepicker2=&everything=cancer&facet=false&financial_disclosure=&fl=id,pmid,publication_date,received_date,accepted_date,title,cross_published_journal_name,author_display,editor_display,article_type,affiliate,subject,financial_disclosure&fq=!article_type_facet:%22Issue%20Image%22&hl=false&institution=&publication_days_ago=-1&q=parameters:utf8=%E2%9C%93&rows=25&subject=&wt=json"
+    ).to_return(File.open('test/fixtures/api_plos_cancer_search.raw'))
+    get :"add_articles", parameters: "utf8=✓&everything=cancer&author=&author_country=&institution=&publication_days_ago=-1&datepicker1=&datepicker2=&subject=&cross_published_journal_name=All+Journals&financial_disclosure=&commit=Search"
+
+    assert_response :success
+
+  end
 end
