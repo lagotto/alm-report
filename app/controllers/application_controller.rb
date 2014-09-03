d74907ade202f24adbfdf292248edb626163977b
Martin Fenner <mfenner@plos.org>
2014-09-03 14:59:07 +0200

    removed functionality to display errors differently for internal IPs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,23 +9,23 @@ end
 # per report.  Controller code should read and write through this interface instead
 # of the session directly.
 class SavedDois
-  
+
   attr_reader :saved
-  
-  
+
+
   def initialize(session_data)
     @saved = session_data
     if @saved.nil?
       @saved = {}
     end
   end
-  
-  
+
+
   def [](x)
     return @saved[x]
   end
-  
-  
+
+
   def []=(x, val)
     if @saved.length >= APP_CONFIG["article_limit"]
       raise DoiLimitReachedError, "Reached limit of #{APP_CONFIG["article_limit"]} DOIs"
@@ -33,27 +33,27 @@ class SavedDois
       @saved[x] = val
     end
   end
-  
-  
+
+
   def delete(val)
     @saved.delete(val)
   end
-  
-  
+
+
   def clone
     @saved.clone
   end
-  
-  
+
+
   def length
     @saved.length
   end
-  
-  
+
+
   def clear
     @saved = {}
   end
-  
+
 end
 
 
@@ -63,60 +63,51 @@ class ApplicationController < ActionController::Base
 
   before_filter :display_nav
   around_filter :save_session_dois
-  
-  # Render pretty 404 and 500 pages in production only.
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception do |e|
-      logger.error(e.message + "\n " + e.backtrace.join("\n    "))
-      internal_error(e)
-    end
 
+  # Render pretty 404 and 500 pages in production
+  unless Rails.application.config.consider_all_requests_local
     rescue_from ActiveRecord::RecordNotFound,
                 ActionController::RoutingError,
                 ActionController::UnknownController,
-                ActionController::UnknownAction,
+                AbstractController::ActionNotFound,
                 ActionController::MethodNotAllowed, :with => :page_not_found
+
+    # rescue_from StandardError, :with => :internal_error
   end
-  
-  
+
+
   # See comment in routes.rb and https://github.com/rails/rails/issues/671
   # for why this is necessary.
   def routing_error
     raise ActionController::RoutingError.new(params[:path])
   end
 
-  
+
   def page_not_found
     @display_nav = false
     @title = "Page Not Found"
     render :template => "static_pages/page_not_found", :status => 404
   end
-  
-  
-  def internal_error(exception)
+
+
+  def internal_error
     @display_nav = false
     @title = "Internal Error"
-    
-    # I tested on iad-dev-almreport01 (Apache+Passenger+Rails) and request.remote_ip
-    # returns the user's IP, not the IP incoming to rails (127.0.0.1).
-    if IpRanges.is_internal_ip(request.remote_ip)
-      @exception = exception
-    end
     render :template => "static_pages/internal_error", :status => 500
   end
-  
-  
+
+
   # Sets @saved_dois based on the contents of the session, and saves it back to the
   # session after an action is run.
   def save_session_dois
     @saved_dois = SavedDois.new(session[:dois])
-    
+
     yield  # Run the action
-    
+
     session[:dois] = @saved_dois.saved
   end
-  
-  
+
+
   # Sets fields used by the UI for results paging of articles.
   # A precondition of this method is that @total_found is set appropriately.
   def set_paging_vars(current_page, results_per_page=APP_CONFIG["results_per_page"])
@@ -126,7 +117,7 @@ class ApplicationController < ActionController::Base
     @end_result = [@end_result, @total_found].min
   end
   protected :set_paging_vars
-  
+
   # The navigation UI element (1 Select Articles and etc) will not be displayed on the static pages
   # That is the only change for the static pages so having a whole separate layout seemed like an overkill
   # This might change in the future
