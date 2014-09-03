b9b6a647ff8efaf45e6cd5c8e87c3146a494fbd6
Martin Fenner <mfenner@plos.org>
2014-09-03 10:51:55 +0200

    make sure internal_ip_ranges is an array

--- lib/ip_ranges.rb
+++ lib/ip_ranges.rb
@@ -1,16 +1,15 @@
 require "ipaddr"
 
-# Module to keep track of PLOS-internal IP ranges.
-# TODO: make this IPv6-aware if necessary.
+# Module to keep track of internal IP ranges.
 module IpRanges
-  
+
   # RFC 1918 private network ranges that should always be considered internal.
   @@private_ranges = [
       IPAddr.new("10.0.0.0/8"),
       IPAddr.new("172.16.0.0/12"),
       IPAddr.new("192.168.0.0/16")
       ]
-  
+
   # Returns true if the given IP should be considered an internal PLOS IP.
   def self.is_internal_ip(ip)
     if ip == "127.0.0.1"
@@ -18,9 +17,9 @@ module IpRanges
     end
     ranges = []
     ranges += @@private_ranges
-    APP_CONFIG["internal_ip_ranges"].each {|range| ranges << IPAddr.new(range)}
+    Array(APP_CONFIG["internal_ip_ranges"]).each {|range| ranges << IPAddr.new(range)}
     ranges.each{|range| return true if range.include?(ip)}
     false
   end
-  
+
 end
