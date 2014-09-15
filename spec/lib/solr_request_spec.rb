753701d4c4b4b35b46b545c1c5c4640da637861c
Jure Triglav <juretriglav@gmail.com>
2014-09-15 12:56:37 +0200

    Move all remaining tests from tests to specs

--- spec/lib/solr_request_spec.rb
+++ spec/lib/solr_request_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 require 'solr_request'
+# require "date"
 
 describe SolrRequest do
-
   it "queries by pubmed ids" do
 
     pmids = [23717645, 16060722, 12345678901234567890]
@@ -24,7 +24,29 @@ describe SolrRequest do
     data[16060722]["id"].should eq("10.1371/journal.pmed.0020124")
     data[16060722]["pmid"].should eq("16060722")
     data[16060722]["publication_date"].should eq(Date.strptime("2005-08-30T00:00:00Zdddddddddd", "%Y-%m-%dT%H:%M:%SZ"))
+  end
 
+  it "parses date ranges" do
+    start_date, end_date = SolrRequest.parse_date_range("-1", nil, nil)
+    assert_nil(start_date)
+    assert_nil(end_date)
+    assert_nil(SolrRequest.build_date_range(nil, nil))
+
+    start_date, end_date = SolrRequest.parse_date_range(
+      "0",
+      "09-15-2012",
+      "02-28-2013"
+    )
+    assert_equal("[2012-09-15T00:00:00Z TO 2013-02-28T23:59:59Z]",
+        SolrRequest.build_date_range(start_date, end_date))
+
+    Timecop.travel(Date.strptime("2013-03-01", "%Y-%m-%d").to_time)
+    start_date, end_date = SolrRequest.parse_date_range("30", nil, nil)
+    assert_equal("[2013-01-30T00:00:00Z TO 2013-03-01T00:00:00Z]",
+        SolrRequest.build_date_range(start_date, end_date))
+    Timecop.return
+
+    # TODO: test end day before start day and other error cases.
   end
 
   it "validates dois" do
