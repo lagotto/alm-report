e7945dbeb0a62f748a0d288717a0a6a95d22045e
Martin Fenner <mfenner@plos.org>
2014-09-16 20:10:21 +0200

    added controller specs for update_session action

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -45,17 +45,36 @@ describe HomeController do
     end
   end
 
-  # describe "GET update_session" do
-  #   let(:params) { { "article_ids"=>["10.1371/journal.pone.0010031|1270166400",
-  #                                    "10.1371/journal.pmed.0010065|1104192000",
-  #                                    "10.1371/journal.pone.0009584|1268092800"],
-  #                    "mode" =>"REMOVE" } }
-  #   it "handles empty params" do
-  #     @cart = Cart.new
-  #     post :update_session, {}
-  #     expect(response).to eq(2)
-  #   end
-  # end
+  describe "GET update_session" do
+    let(:article_ids) { ["10.1371/journal.pone.0010031|1270166400",
+                         "10.1371/journal.pmed.0010065|1104192000",
+                         "10.1371/journal.pone.0009584|1268092800"] }
+    let(:dois) { { "10.1371/journal.pone.0010031" => 1270166400,
+                   "10.1371/journal.pmed.0010065" => 1104192000,
+                   "10.1371/journal.pone.0009584" => 1268092800 } }
+
+    it "handles params" do
+      post :update_session, { "article_ids" => article_ids, "mode" => "SAVE" }
+      body = JSON.parse(response.body)
+      expect(body).to eq("status" => "success", "delta" => 3)
+      session[:dois].should eq(dois)
+    end
+
+    it "handles empty params" do
+      post :update_session, {}
+      body = JSON.parse(response.body)
+      expect(body).to eq("status" => "success", "delta" => 0)
+      session[:dois].should eq({})
+    end
+
+    it "removes dois" do
+      session[:dois] = dois
+      post :update_session, { "article_ids" => article_ids, "mode" => "REMOVE" }
+      body = JSON.parse(response.body)
+      expect(body).to eq("status" => "success", "delta" => -3)
+      session[:dois].should eq({})
+    end
+  end
 
   describe "parse_article_keys" do
     let(:article_ids) { ["10.1371/journal.pone.0010031|1410868245",
