be0ac70f9a30a9dbeaf397f52a8be2864ea9d9f9
Jure Triglav <juretriglav@gmail.com>
2014-09-23 15:46:41 +0200

    Remove last mentions of BackendService in Report and ReportDoi. Use a safer way to create ReportDois.

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,26 +2,35 @@ require 'csv'
 
 class Report < ActiveRecord::Base
   attr_accessible :user_id
-  
+
   # TODO: figure out what we want the default sort order to be.
   # doi is likely not a good solution.
   has_many :report_dois, :order => 'sort_order'
-  
-  
+
+
   # Creates a child ReportDoi object for each DOI passed in in the input array.
   # Sort order is determined by the position in the array.  This object must have
   # already been saved to the DB before this method is called.
   def add_all_dois(dois)
-    
+
     # Since reports can have many DOIs, for performance we do a batch insert.
     # Active Record won't do this on its own.
-    sql = "INSERT report_dois(doi, report_id, sort_order, created_at, updated_at) VALUES "
-    dois.each_with_index {|doi, i| sql << "('#{doi}', #{self.id}, #{i}, NOW(), NOW()), "}
-    sql[-2] = ";"
-    self.connection.execute(sql)
+    report_dois = dois.map.with_index do |doi, index|
+      {
+        doi: doi,
+        sort_order: index
+      }
+    end
+    self.report_dois.create(report_dois)
+
+    # TEMP DISABLE, PRETTY BAD.
+    # sql = "INSERT report_dois(doi, report_id, sort_order, created_at, updated_at) VALUES "
+    # dois.each_with_index {|doi, i| sql << "('#{doi}', #{self.id}, #{i}, NOW(), NOW()), "}
+    # sql[-2] = ";"
+    # self.connection.execute(sql)
   end
-  
-  
+
+
   # Sets the @sorted_report_dates field.
   # Precondition: load_articles_from_solr has already been called.
   def sort_report_dates
@@ -31,16 +40,16 @@ class Report < ActiveRecord::Base
     end
   end
   private :sort_report_dates
-  
-  
+
+
   # Returns the earliest publication date of any article in this report.
   # Precondition: load_articles_from_solr has already been called.
   def get_earliest_report_date
     sort_report_dates
     @sorted_report_dates[0]
   end
-  
-  
+
+
   # Returns the latest publication date of any article in this report.
   # Precondition: load_articles_from_solr has already been called.
   def get_latest_report_date
@@ -55,7 +64,7 @@ class Report < ActiveRecord::Base
     if (field.nil?)
 
       alm_data = AlmRequest.get_data_for_articles(report_dois)
-      solr_data = BackendService.get_article_data_for_list_display(report_dois)
+      solr_data = report_dois.map { |doi| SearchResult.from_cache(doi) }
 
       CSV.generate({ :force_quotes => true }) do | csv |
         title_row = [
@@ -72,12 +81,12 @@ class Report < ActiveRecord::Base
 
           article_alm_data = alm_data[report_doi.doi]
           article_data = solr_data[report_doi.doi]
-          
+
           # If the article was unpublished (rare), skip it.
           if !article_alm_data.nil? && !article_data.nil?
             row = [
                 report_doi.doi, article_data["pmid"], article_data["publication_date"],
-                
+
                 # Some of the long free-form text fields can contain newlines; convert
                 # these to spaces.
                 article_data["title"].gsub(/\n/, ' '),
@@ -110,8 +119,8 @@ class Report < ActiveRecord::Base
       end
     end
   end
-  
-  
+
+
   # Returns a string suitable for inclusion in the CSV report for subject areas
   # of an article.  Only "leaf" or lowest-level categories are included.  The
   # input is the list of subjects as returned by solr.
@@ -119,13 +128,13 @@ class Report < ActiveRecord::Base
     if subject_list.nil?
       ""
     else
-      
+
       # We sort on the leaf categories, just like ambra does.
       subject_list.collect{|subject| subject.split("/")[-1]}.sort.uniq.join(",")
     end
   end
-  
-  
+
+
   # Joins fields together for inclusion in a single CSV field.
   #
   # Params:
@@ -136,12 +145,12 @@ class Report < ActiveRecord::Base
     result = fields.nil? ? "" : fields.join(delimiter)
     result.gsub(/\n/, ' ')
   end
-  
-  
+
+
   # Returns the given field from a solr data structure for an article, or the
   # empty string if the field does not exist.
   def get_optional_field(article_data, field_name)
     article_data[field_name].nil? ? "" : article_data[field_name]
   end
-  
+
 end
--- app/models/report_doi.rb
+++ app/models/report_doi.rb
@@ -2,11 +2,4 @@ class ReportDoi < ActiveRecord::Base
   belongs_to :report
   attr_accessible :doi, :sort_order
   attr_accessor :solr, :alm, :display_index
-  
-  # Returns true if this ReportDoi corresponds to a PLOS Currents article, and
-  # false otherwise.
-  def is_currents_doi
-    BackendService.is_currents_doi(doi)
-  end
-
 end
