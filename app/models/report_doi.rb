ad55bf0c3ad93b3168a574fe61b4020623f18c68
Jure Triglav <juretriglav@gmail.com>
2014-10-07 14:29:14 +0200

    Simplify ReportDoi CSV generation.

--- app/models/report_doi.rb
+++ app/models/report_doi.rb
@@ -9,29 +9,8 @@ class ReportDoi < ActiveRecord::Base
 
   def to_csv
     if alm && solr
-      row = [
-        doi,
-        solr.pmid,
-        solr.publication_date,
-        solr.title,
-        solr.authors,
-        solr.affiliates.try(:join, "; ")
-      ]
-
-      AlmRequest.ALM_METRICS.keys.each do |metric|
-        row.push(alm[metric])
-      end
-
-      row += [
-        solr.journal,
-        solr.article_type,
-        solr.financial_disclosure,
-        subject_string,
-        solr.received_date,
-        solr.accepted_date,
-        solr.editors.try(:join, ", "),
-        "http://dx.doi.org/#{doi}"
-      ]
+      alm_columns = AlmRequest.ALM_METRICS.keys.map { |metric| alm[metric] }
+      row = csv_row.insert(6, *alm_columns)
       # Some of the long free-form text fields can contain newlines; convert
       # these to spaces.
       row.map do |field|
@@ -41,6 +20,26 @@ class ReportDoi < ActiveRecord::Base
   end
 
   private
+
+  def csv_row
+    [
+      doi,
+      solr.pmid,
+      solr.publication_date,
+      solr.title,
+      solr.authors,
+      solr.affiliates.try(:join, "; "),
+      # Here be ALM data
+      solr.journal,
+      solr.article_type,
+      solr.financial_disclosure,
+      subject_string,
+      solr.received_date,
+      solr.accepted_date,
+      solr.editors.try(:join, ", "),
+      "http://dx.doi.org/#{doi}"
+    ]
+  end
 
   # Returns a string suitable for inclusion in the CSV report for subject areas
   # of an article.  Only "leaf" or lowest-level categories are included.  The
