2c2f0110fc3a01d6465169b2019335019453c2fb
Jure Triglav <juretriglav@gmail.com>
2014-11-25 16:49:11 +0100

    Remove manual (and absolute path require for bower_rails)

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ end
 
 module AlmReport
   class Application < Rails::Application
-    require "/vagrant/config/initializers/bower_rails.rb"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
