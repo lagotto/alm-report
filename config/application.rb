11f04e1e7478acf44b007045b355c6da6abfe1c6
Martin Fenner <mfenner@plos.org>
2014-12-03 07:27:59 -0800

    use utf-8 encoding

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,8 @@ module AlmReport
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths << Rails.root.join('lib')
 
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+    # Configure the default encoding used in templates for Ruby.
+    config.encoding = "utf-8"
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
