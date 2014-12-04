ca8f5494371ae3b43e0c89d507f256ceb068dde7
Jure Triglav <juretriglav@gmail.com>
2014-12-04 21:18:38 +0100

    Retransform keys from base + id back to id form.

--- app/models/concerns/cacheable.rb
+++ app/models/concerns/cacheable.rb
@@ -1,21 +1,19 @@
-require "pry"
 module Cacheable
   extend ActiveSupport::Concern
 
   module ClassMethods
     def cache(ids, opts = {}, &block)
       base = "#{self.name}:#{caller_locations(1,1)[0].label}:"
-      missing = ids.map { |id| base + id }
 
+      missing = ids.map { |id| base + id }
       found = Rails.cache.read_multi(*missing)
       missing -= found.keys
 
       unless missing.empty?
-        results = yield(missing)
+        results = yield(missing.map { |id| id[base.length..-1] })
 
         results = results.map do |result|
-          Rails.cache.write(base + result.id, result)
-          # Make sure we only get requested
+          Rails.cache.write(base + result.id, result, opts)
           result if ids.include? result.id
         end.compact
       end
