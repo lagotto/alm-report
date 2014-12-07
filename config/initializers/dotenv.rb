2e378a5c1bf3eb1d35d4139fbb486aa96ebbf0c1
Jure Triglav <juretriglav@gmail.com>
2014-12-07 22:44:49 +0100

    Remove interpolation from .env.travis, and change API_KEY env parameter to ALM_API_KEY

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,4 +1,4 @@
 # Check for required ENV variables, can be set in .env file
 
-env_vars = %w{ DB_USERNAME DB_HOST HOSTNAME SERVERS SITENAME API_KEY SECRET_KEY_BASE MODE SEARCH SOLR_URL ALM_URL }
+env_vars = %w{ DB_USERNAME DB_HOST HOSTNAME SERVERS SITENAME ALM_API_KEY SECRET_KEY_BASE MODE SEARCH SOLR_URL ALM_URL }
 env_vars.each { |env| fail ArgumentError,  "ENV[#{env}] is not set" if ENV[env].blank? }
