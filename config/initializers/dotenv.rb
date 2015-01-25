1fcfecd0cb0b2f2c36c0c9b019b52686f8904136
Martin Fenner <mfenner@plos.org>
2015-01-25 17:49:20 +0100

    include SERVERS ENV variable for travis

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,5 +1,5 @@
 # Check for required ENV variables, can be set in .env file
 # ENV_VARS is hash of required ENV variables
-env_vars = %w{ DB_USERNAME DB_HOST HOSTNAME SERVERS SITENAME SECRET_KEY_BASE MODE SEARCH SOLR_URL SOLR_MAX_DOIS ALM_URL ALM_API_KEY OMNIAUTH }
+env_vars = %w{ DB_USERNAME DB_HOST HOSTNAME MEMCACHE_SERVERS SITENAME SECRET_KEY_BASE MODE SEARCH SOLR_URL SOLR_MAX_DOIS ALM_URL ALM_API_KEY OMNIAUTH }
 env_vars.each { |env| fail ArgumentError,  "ENV[#{env}] is not set" if ENV[env].blank? }
 ENV_VARS = Hash[env_vars.map { |env| [env, ENV[env]] }]
