c10af2b601a5936dcb3796cf7d8efed178622d47
Martin Fenner <mfenner@plos.org>
2015-01-26 19:41:24 +0100

    don't include bower assets in initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
   charts.js print.css html5shiv.js application.plos.css alm_report.js
-  lodash/dist/lodash.js d3-tip/index.js bubble/app/scripts/bubble_chart.js
-  sunburst/app/scripts/sunburst_chart.js line/app/scripts/line_chart.js datamaps/dist/datamaps.world.js
 )
 
 Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bower_components")
