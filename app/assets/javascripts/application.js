df579dbc6f34adeaee2567cdc830bacbf699b7b9
Martin Fenner <mfenner@plos.org>
2015-07-20 21:51:15 +0200

    updated manifest for js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,14 +7,16 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
-//= require jquery_ujs
+// Load bower components
+//= require es5-shim/es5-shim.js
+//= require lodash/lodash.js
+//= require jquery/dist/jquery.js
+//= require jquery-ujs/src/rails.js
+//= require jquery-ui
 //= require jquery-ui
 //= require query-string
 //= require script
 //= require advancedSearch
-//= require es5_shim
-//= require lodash/dist/lodash.js
 //= require d3-tip/index
 //= require bubble/app/scripts/bubble_chart
 //= require sunburst/app/scripts/sunburst_chart
