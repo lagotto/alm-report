d2ed3b7e3656ed842bad43516c2a92fb9a7daac0
Martin Fenner <mfenner@plos.org>
2015-07-21 00:41:10 +0200

    include print.css. #157

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( application.plos.css alm_report.js )
+Rails.application.config.assets.precompile += %w( application.plos.css print.css alm_report.js )
 
 Rails.application.config.assets.paths << Rails.root.join("vendor","assets","bower_components")
